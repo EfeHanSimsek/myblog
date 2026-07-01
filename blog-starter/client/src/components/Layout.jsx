ff5362d4d021bb753d23cd9860939b5085f3aaa1
W0rtex <260776832+EfeHanSimsek@users.noreply.github.com>
2026-07-01 22:44:01 +0300

    Fix React JSX runtime imports in layout

--- blog-starter/client/src/components/Layout.jsx
+++ blog-starter/client/src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, NavLink, Outlet, useNavigate } from 'react-router-dom';
 import { clearToken, getToken } from '../lib/api';
 
